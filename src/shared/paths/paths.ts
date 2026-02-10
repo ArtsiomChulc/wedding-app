82d54d8b5478e847ccb06b38c58cb527c1c2f302
Artsiom <artemorsha@mail.ru>
2026-02-10 23:02:12 +0300

    add: paths

---
+++ src/shared/paths/paths.ts
@@ -0,0 +1,10 @@
+import { PathsType } from '@/shared/types/pahtsType';
+
+export const paths: PathsType = {
+    home: '/',
+    weddings: '/weddings',
+    corporate_event: '/corporate-event',
+    other_event: '/other-event',
+    contacts: '/contacts',
+    agencies: '/agencies',
+};
---
+++ src/shared/types/pahtsType.ts
@@ -0,0 +1,8 @@
+export type PathsType = {
+    home: string;
+    weddings: string;
+    corporate_event: string;
+    other_event: string;
+    contacts: string;
+    agencies: string;
+};
