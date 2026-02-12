d98cefc9d5e2ee35b3f1c1258745015c051116a7
Artsiom <artemorsha@mail.ru>
2026-02-12 16:05:04 +0300

    fix: layout in app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,28 @@
 import type { Metadata } from 'next';
-import Container from '@/components/ui/container/Container';
 import { ReactNode } from 'react';
 import { inter, libertinus } from '@/app/fonts/fonts';
 import './globals.css';
-import Header from '@/components/ui/header/Header';
 
 export const metadata: Metadata = {
-    title: 'Веду свадьбы',
-    description: 'Ведущий на свадьбу Минск',
+    title: 'Владислав Рынкевич | Ведущий',
+    icons: {
+        icon: [
+            { url: '/favicon.ico', sizes: '72x72', type: 'image/png' },
+        ],
+        apple: [
+            { url: '/apple-icon.png', sizes: '180x180', type: 'image/png' },
+        ],
+    },
 };
 
-export default function RootLayout({
+export default function ErrorLayout({
     children,
 }: Readonly<{
     children: ReactNode;
 }>) {
     return (
         <html lang='ru' className={`${inter.variable} ${libertinus.variable}`}>
-            <body>
-                <Container>
-                    <Header />
-                    {children}
-                </Container>
-            </body>
+            <body>{children}</body>
         </html>
     );
 }
