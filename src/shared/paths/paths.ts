106dcb6ddd6e1a46999f075bf42d8b8e258d6923
Artsiom <artemorsha@mail.ru>
2026-02-12 16:06:44 +0300

    refactor: path

--- src/shared/paths/paths.ts
+++ src/shared/paths/paths.ts
@@ -3,7 +3,7 @@ import { PathsType } from '@/shared/types/pahtsType';
 export const paths: PathsType = {
     home: '/',
     weddings: '/weddings',
-    corporate_event: '/corporate-event',
+    'corporate-event': '/corporate-event',
     other_event: '/other-event',
     contacts: '/contacts',
     agencies: '/agencies',
--- src/shared/types/pahtsType.ts
+++ src/shared/types/pahtsType.ts
@@ -1,7 +1,7 @@
 export type PathsType = {
     home: string;
     weddings: string;
-    corporate_event: string;
+    'corporate-event': string;
     other_event: string;
     contacts: string;
     agencies: string;
