65cac9819cf80591d19f9cbcd9acecf93686b8e8
Artsiom <artemorsha@mail.ru>
2026-02-16 16:20:22 +0300

    fix: use desc block

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,12 @@
 import MainBlock from '@/components/ui/mainBlock/MainBlock';
+import DescBlock from '@/components/ui/descBlock/DescBlock';
+import s from './MainPage.module.css';
 
 export default function Home() {
-    return <div>
-        <MainBlock/>
-    </div>;
+    return (
+        <div className={s.main_page}>
+            <MainBlock />
+            <DescBlock />
+        </div>
+    );
 }
